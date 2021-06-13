19bdd2c54c21ee4d801de07cb618b62fced48a0f
Si3ver <izhouwl@163.com>
2021-06-14 03:17:06 +0800

    feat: sideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Header, Footer } from "./components";
+import { Col, Row } from "antd";
+import { Header, Footer, SideMenu } from "./components";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles["page-content"]}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <div style={{ background: "blue" }}>走马灯</div>
+          </Col>
+        </Row>
+      </div>
       <Footer />
     </div>
   );
